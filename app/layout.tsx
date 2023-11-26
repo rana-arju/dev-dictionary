bedaeed5e7beea326c6a62f91386b6708bb510a0
rana-arju <arjurana20@gmail.com>
2023-11-27 01:15:59 +0600

    lint error solve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
@@ -5,17 +6,19 @@ import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import ThemeProvider from "@/context/ThemeProvider";
 
+
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
+
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-spaceGrotesk",
 });
-
 export const metadata: Metadata = {
   title: "Dev Flow",
   description:
