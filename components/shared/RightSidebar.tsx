22026c61be1b98eb6e293a66649017e5ec6eb081
rana-arju <arjurana20@gmail.com>
2023-12-02 21:56:39 +0600

    hot question show on sidebar api call and show from database

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,31 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-function RightSidebar() {
-  const hotQuestion = [
-    {
-      _id: "1",
-      title: "Is it only me or the font is bolder than necessary?",
-    },
-    {
-      _id: "2",
-      title:
-        "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-    },
-    {
-      _id: "3",
-      title: "Can I get the course for free?",
-    },
-    {
-      _id: "4",
-      title: "Redux Toolkit Not Updating State as Expected",
-    },
-    {
-      _id: "5",
-      title: "Async/Await Function Not Handling Errors Properly",
-    },
-  ];
+async function RightSidebar() {
+  
   const popularTags = [
     {
       _id: "1",
@@ -53,6 +32,8 @@ function RightSidebar() {
       totalQuestion: 14,
     },
   ];
+
+  const hotQuestion = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
       <div>
