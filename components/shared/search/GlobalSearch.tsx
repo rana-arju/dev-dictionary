070b0123da435b47dc5170902cb5d1a07bf2e307
rana-arju <arjurana20@gmail.com>
2023-12-07 00:43:01 +0600

    global search functionality implement

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,14 +1,84 @@
+"use client";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import GlobalResult from "./GlobalResult";
 
 function GlobalSearch() {
-    
+  const searchContainerRef = useRef(null);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const handleOutSideClick = (event: any) => {
+      if (
+        searchContainerRef.current &&
+        // @ts-ignore
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+        setSearch("");
+      }
+    };
+    setIsOpen(false);
+    document.addEventListener("click", handleOutSideClick);
+    return () => {
+      document.removeEventListener("click", handleOutSideClick);
+    };
+  }, [pathname]);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [pathname, router, search, searchParams, query]);
+
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div
+      className="relative w-full max-w-[600px] max-lg:hidden"
+      ref={searchContainerRef}
+    >
       <div className="background-light800_darkgradient relative flex min-h-[56px] items-center gap-1 rounded-xl px-4">
-        <Image src={"/assets/icons/search.svg"} height={24} width={24} alt="search" className="cursor-pointer" />
-        <Input type="text" placeholder="Search globally" className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none " />
+        <Image
+          src={"/assets/icons/search.svg"}
+          height={24}
+          width={24}
+          alt="search"
+          className="cursor-pointer"
+        />
+        <Input
+          type="text"
+          placeholder="Search globally"
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light700 border-none shadow-none outline-none"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "" && isOpen) setIsOpen(false);
+          }}
+        />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 }
--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -124,3 +124,7 @@ export interface EditQuestionParams {
   content: string;
   path: string;
 }
+export interface SearchParams {
+  query?: string | null;
+  type?: string | null;
+}
